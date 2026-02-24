eec0ca1ccfd1c59ff766cd2297a5f47d41b01f73
Augusto Luna <lunaugust@gmail.com>
2026-02-24 12:19:19 -0300

    feat: register service worker for PWA support

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 import "./styles/global.css";
 import App from "./App";
 import { I18nProvider } from "./i18n";
+
+registerSW({
+  immediate: true,
+  onRegisteredSW(_, registration) {
+    registration?.update();
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
